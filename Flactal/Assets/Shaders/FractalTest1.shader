162612553dc938e53ae0b75abd86a7e35ea73950
ER0WA <erowaongame@gmail.com>
2021-06-07 03:32:37 +0900

    フラクタルとジュリア集合の簡易プログラム

---
+++ Flactal/Assets/Shaders/FractalTest1.shader
@@ -0,0 +1,85 @@
+﻿Shader "Unlit/FractalTest1"
+{
+	SubShader{
+		Tags{ "RenderType" = "Transparent" "Queue" = "Transparent" }
+		Blend OneMinusDstColor One //OneMinusDstColor
+
+		Pass{
+		CGPROGRAM
+#pragma vertex vert_img
+#pragma fragment frag
+#pragma target 3.0
+
+#include "UnityCG.cginc"
+
+
+	float complexity = 29.0;
+	float test = 1.0;
+	float blueMode = -1.0;
+
+	
+
+
+	float4 frag(v2f_img i) : COLOR{
+	float2 mcoord;
+	float2 coord = float2(0.0,0.0);
+	mcoord.x = ((1.0 - i.uv.x)*3.5) - 2.5;
+	mcoord.y = (i.uv.y*2.0) - 1.0;
+	float iteration = 0.0;
+	float _MaxIter = 29.0;
+	const float PI = 3.14159;
+	float xtemp;
+	float4 color;
+
+	
+
+	for (iteration = 0.0; iteration < _MaxIter; iteration += 1.0) {
+		if (coord.x*coord.x + coord.y*coord.y > 2.0*(cos(fmod(complexity, 2.0*PI)) + 1.0))
+			break;
+		xtemp = coord.x*coord.x - coord.y*coord.y + mcoord.x;
+		coord.y = 2.0*coord.x*coord.y + mcoord.y;
+		coord.x = xtemp;
+	}
+	//float val = fmod((iteration / _MaxIter) + _Time.x,1.0);
+	float val = fmod((iteration / _MaxIter) + complexity, 1.0);
+	
+	color.a = 1.0;
+
+	if (blueMode < 0.0)
+	{
+
+		color.b = clamp(complexity / PI + 0.2, 0.1, 1.0);
+		color.r = clamp((abs(fmod(val, 1.0) - 0.5)), 0.2, 0.6);
+		color.g = clamp((abs(fmod(val, 1.0) - 0.5)), 0.1, 0.2);
+
+		if (abs(fmod(iteration, 2.0)) < 1.0)
+		{
+			color.b = 0.0;
+			color.r = 0.0;
+			color.g = 0.0;
+		}
+	}
+	else
+	{
+
+
+		color.r = clamp((3.0*abs(fmod(2.0*val, 1.0) - 0.5)), 0.0, 1.0);
+		color.g = clamp((3.0*abs(fmod(2.0*val + (1.0 / 3.0), 1.0) - 0.5)), 0.0, 1.0);
+		color.b = clamp((3.0*abs(fmod(2.0*val - (1.0 / 3.0), 1.0) - 0.5)), 0.0, 1.0);
+	}
+	
+	
+	if (iteration < (_MaxIter / 7.0))
+	{
+		color.r = 0.0;
+		color.g = 0.0;
+		color.b = 0.0;
+		color.a = 0.0;
+	}
+
+	return color;
+	}
+		ENDCG
+	}
+	}
+}
---
+++ Flactal/Assets/UIManager.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField]
+    Renderer[] FractalRenderer = new Renderer[4];
+
+    [SerializeField]
+    Slider complicateSlider;
+
+    [SerializeField]
+    Toggle blueMode;
+
+    [SerializeField]
+    Renderer JuliaRenderer;
+
+    [SerializeField]
+    Slider juliaComplicateSlider;
+
+    [SerializeField]
+    Slider julia_Threshold;
+    
+
+
+
+    public float  test = 1.0f;
+    public float _MaxIter = 1.0f;
+
+    const float PI = 3.14159f;
+
+    private Material[] fractalMaterial = new Material[4];
+    private Material juliaMaterial;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        /// flactal
+        complicateSlider.maxValue = PI-0.2f;
+        complicateSlider.minValue = 0.0f;
+        for (int i = 0; i < 4; ++i)
+        {
+            fractalMaterial[i] = FractalRenderer[i].material;
+        }
+
+        /// julia
+        juliaComplicateSlider.maxValue = 256.0f;
+        juliaComplicateSlider.minValue = 1.0f;
+        juliaComplicateSlider.value = 16;
+
+        julia_Threshold.maxValue = 200.0f;
+        julia_Threshold.minValue = 1.0f;
+        julia_Threshold.value = 2.0f;
+        juliaMaterial = JuliaRenderer.material;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        for (int i = 0; i < 4; ++i)
+        {
+            if (fractalMaterial[i] != null)
+            {
+                _MaxIter = fractalMaterial[i].GetFloat("_MaxIter");
+                fractalMaterial[i].SetFloat("_MaxIter", 10.0f);
+                fractalMaterial[i].SetFloat("complexity", Mathf.Min(complicateSlider.value, 2.0f * PI));
+                fractalMaterial[i].SetFloat("test", test);
+
+                if (blueMode.isOn)
+                {
+                    fractalMaterial[i].SetFloat("blueMode", 1.0f);
+                }else
+                {
+                    fractalMaterial[i].SetFloat("blueMode", -1.0f);
+                }
+
+            }
+        }
+
+
+        ///
+        juliaMaterial.SetInt("_MaxIteration", (int)juliaComplicateSlider.value);
+        juliaMaterial.SetInt("_Threshold", (int)julia_Threshold.value);
+
+
+
+
+    }
+}
